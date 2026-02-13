397264af50a09b807bc6c59a7a7f66cbc01adc56
ushankamesh-devX <eg245099@engug.ruh.ac.lk>
2026-02-14 04:25:32 +0530

    feat(db): add phpmyadmin service and open port 8081

--- infra/main.tf
+++ infra/main.tf
@@ -169,6 +169,14 @@ resource "aws_security_group" "ec2" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "phpMyAdmin from anywhere"
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     description = "Allow all outbound"
     from_port   = 0
